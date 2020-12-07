e0b4a313ff4d71723c53499da101323f4f7977d6
Luminiscental <luminiscental01@gmail.com>
2020-12-07 10:07:06 +0000

    Cleanup day 7 slightly

--- src/day7.rs
+++ src/day7.rs
@@ -1,5 +1,5 @@
 use crate::day::Day;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 fn take_words(count: usize, string: &str) -> (&str, &str) {
     string
@@ -14,6 +14,7 @@ fn take_words(count: usize, string: &str) -> (&str, &str) {
 #[derive(Debug)]
 pub struct RuleSet<'a> {
     child_map: HashMap<&'a str, Vec<(usize, &'a str)>>,
+    parent_map: HashMap<&'a str, HashSet<&'a str>>,
 }
 
 pub struct Day7 {}
@@ -27,61 +28,50 @@ impl<'a> Day<'a> for Day7 {
     const INDEX: usize = 7;
 
     fn parse(raw_input: &'a str) -> Self::Input1 {
+        let mut child_map = HashMap::new();
+        let mut parent_map = HashMap::new();
+        for line in raw_input.lines().filter(|line| !line.is_empty()) {
+            let (parent_color, rest) = take_words(2, line);
+            let (_, children) = take_words(2, rest);
+            if children.starts_with("no") {
+                child_map.insert(parent_color, Vec::new());
+            } else {
+                let mut children = children;
+                while !children.is_empty() {
+                    let (child, remaining_children) = take_words(4, children);
+                    let (child_count, rest) = take_words(1, child);
+                    let child_count = child_count.parse::<usize>().unwrap();
+                    let (child_color, _) = take_words(2, rest);
+                    child_map
+                        .entry(parent_color)
+                        .or_insert_with(Vec::new)
+                        .push((child_count, child_color));
+                    parent_map
+                        .entry(child_color)
+                        .or_insert_with(HashSet::new)
+                        .insert(parent_color);
+                    children = remaining_children;
+                }
+            };
+        }
         RuleSet {
-            child_map: raw_input
-                .lines()
-                .filter(|line| !line.is_empty())
-                .map(|line| {
-                    let (parent_color, rest) = take_words(2, line);
-                    let (_, children) = take_words(2, rest);
-                    let children = if children.starts_with("no") {
-                        Vec::new()
-                    } else {
-                        let mut children = children;
-                        let mut vec = Vec::new();
-                        while !children.is_empty() {
-                            let (child, remaining_children) = take_words(4, children);
-                            let (child_count, rest) = take_words(1, child);
-                            let (child_color, _) = take_words(2, rest);
-                            vec.push((child_count.parse::<usize>().unwrap(), child_color));
-                            children = remaining_children;
-                        }
-                        vec
-                    };
-                    (parent_color, children)
-                })
-                .collect(),
+            child_map,
+            parent_map,
         }
     }
 
     fn solve_part1(input: Self::Input1) -> (Self::Input2, Self::Output1) {
-        let mut contains_map = HashMap::new();
-        contains_map.insert("shiny gold", true);
-        while contains_map.len() < input.child_map.len() {
-            for (color, children) in input.child_map.iter() {
-                if !contains_map.contains_key(color) {
-                    if children.is_empty() {
-                        contains_map.insert(color, false);
-                    } else if let Some(containment) = children
-                        .iter()
-                        .map(|pair| contains_map.get(&pair.1))
-                        // if any Some(true), result is Some(true)
-                        // if all Some(false), result is Some(false)
-                        // else result is None
-                        .fold(Some(false), |acc, child| match (acc, child) {
-                            (Some(true), _) => Some(true),
-                            (_, Some(true)) => Some(true),
-                            (acc, Some(false)) => acc,
-                            (_, None) => None,
-                        })
-                    {
-                        contains_map.insert(color, containment);
-                    }
+        let mut ancestor_set = HashSet::new();
+        let mut stack = vec!["shiny gold"];
+        while let Some(color) = stack.pop() {
+            ancestor_set.insert(color);
+            if let Some(parents) = input.parent_map.get(color) {
+                for parent_color in parents.iter() {
+                    stack.push(parent_color);
                 }
             }
         }
-        let count = contains_map.iter().filter(|pair| *pair.1).count() - 1;
-        (input, count)
+        (input, ancestor_set.len() - 1)
     }
 
     fn solve_part2(input: Self::Input2) -> Self::Output1 {
