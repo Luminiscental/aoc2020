303c3c75677bcc2d02de316f1c1fdecee43fa062
Luminiscental <luminiscental01@gmail.com>
2020-12-12 09:53:04 +0000

    Remove unnecessary enum

--- src/day12.rs
+++ src/day12.rs
@@ -1,32 +1,4 @@
 use crate::day::Day;
-use std::str::FromStr;
-
-pub enum Direction {
-    North,
-    South,
-    East,
-    West,
-    Left,
-    Right,
-    Forward,
-}
-
-impl FromStr for Direction {
-    type Err = ();
-
-    fn from_str(s: &str) -> Result<Self, ()> {
-        match s {
-            "N" => Ok(Self::North),
-            "E" => Ok(Self::East),
-            "S" => Ok(Self::South),
-            "W" => Ok(Self::West),
-            "L" => Ok(Self::Left),
-            "R" => Ok(Self::Right),
-            "F" => Ok(Self::Forward),
-            _ => Err(()),
-        }
-    }
-}
 
 fn rotate(point_x: &mut i32, point_y: &mut i32, angle: i32) {
     let (cos, sin) = match angle {
@@ -41,41 +13,42 @@ fn rotate(point_x: &mut i32, point_y: &mut i32, angle: i32) {
     *point_y = new_facing_y;
 }
 
-pub struct Day12 {}
-
-fn final_ship_distance(
-    actions: &[(Direction, i32)],
+fn final_ship_distance<'a>(
+    actions: &[(&'a str, i32)],
     mut waypoint_x: i32,
     mut waypoint_y: i32,
     move_waypoint: bool,
 ) -> i32 {
     let (mut offset_x, mut offset_y) = (0, 0);
     for (dir, dist) in actions.iter() {
-        match dir {
-            Direction::North if move_waypoint => waypoint_y += dist,
-            Direction::East if move_waypoint => waypoint_x += dist,
-            Direction::South if move_waypoint => waypoint_y -= dist,
-            Direction::West if move_waypoint => waypoint_x -= dist,
+        match *dir {
+            "N" if move_waypoint => waypoint_y += dist,
+            "E" if move_waypoint => waypoint_x += dist,
+            "S" if move_waypoint => waypoint_y -= dist,
+            "W" if move_waypoint => waypoint_x -= dist,
 
-            Direction::North => offset_y += dist,
-            Direction::East => offset_x += dist,
-            Direction::South => offset_y -= dist,
-            Direction::West => offset_x -= dist,
+            "N" => offset_y += dist,
+            "E" => offset_x += dist,
+            "S" => offset_y -= dist,
+            "W" => offset_x -= dist,
 
-            Direction::Forward => {
+            "F" => {
                 offset_x += waypoint_x * dist;
                 offset_y += waypoint_y * dist;
             }
-            Direction::Left => rotate(&mut waypoint_x, &mut waypoint_y, *dist),
-            Direction::Right => rotate(&mut waypoint_x, &mut waypoint_y, 360 - dist),
+            "L" => rotate(&mut waypoint_x, &mut waypoint_y, *dist),
+            "R" => rotate(&mut waypoint_x, &mut waypoint_y, 360 - dist),
+            _ => unreachable!(),
         }
     }
     offset_x.abs() + offset_y.abs()
 }
 
+pub struct Day12 {}
+
 impl<'a> Day<'a> for Day12 {
-    type Input1 = Vec<(Direction, i32)>;
-    type Input2 = Vec<(Direction, i32)>;
+    type Input1 = Vec<(&'a str, i32)>;
+    type Input2 = Vec<(&'a str, i32)>;
     type Output1 = i32;
     type Output2 = i32;
 
@@ -85,12 +58,7 @@ impl<'a> Day<'a> for Day12 {
         raw_input
             .lines()
             .filter(|line| !line.is_empty())
-            .map(|line| {
-                (
-                    line[0..1].parse::<Direction>().unwrap(),
-                    line[1..].parse::<i32>().unwrap(),
-                )
-            })
+            .map(|line| (&line[0..1], line[1..].parse::<i32>().unwrap()))
             .collect()
     }
 
