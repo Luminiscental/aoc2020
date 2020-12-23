6bec2a90ac6c96824a8dc661ce44bb018056b10d
Luminiscental <luminiscental01@gmail.com>
2020-12-23 13:54:30 +0000

    Refactor day 23

--- src/day23.rs
+++ src/day23.rs
@@ -1,4 +1,5 @@
 use crate::day::Day;
+use std::iter;
 
 fn dec_cup(cup: usize, cup_count: usize) -> usize {
     match cup {
@@ -7,69 +8,75 @@ fn dec_cup(cup: usize, cup_count: usize) -> usize {
     }
 }
 
-struct IterCups<'a> {
-    current: usize,
-    next: &'a Vec<usize>,
-}
-
-impl<'a> Iterator for IterCups<'a> {
-    type Item = usize;
-
-    fn next(&mut self) -> Option<usize> {
-        let item = Some(self.current);
-        self.current = self.next[self.current - 1];
-        item
-    }
-}
-
-#[derive(Debug, Clone)]
 pub struct Cups {
     current: usize,
     next: Vec<usize>,
 }
 
-impl Cups {
-    fn from_slice(slice: &[usize]) -> Self {
-        let current = slice[0];
-        let mut next: Vec<_> = slice.into();
-        for (value, next_value) in slice.iter().copied().zip(slice.iter().copied().skip(1)) {
-            next[value - 1] = next_value;
-        }
-        next[slice[slice.len() - 1] - 1] = slice[0];
-        Cups { current, next }
-    }
+impl Iterator for Cups {
+    type Item = usize;
 
-    fn next(&self, cup: usize) -> usize {
+    fn next(&mut self) -> Option<usize> {
+        let item = self.current;
+        self.current = self.next_cup(self.current);
+        Some(item)
+    }
+}
+
+impl Cups {
+    fn next_cup(&self, cup: usize) -> usize {
         self.next[cup - 1]
     }
 
-    fn set_next(&mut self, cup: usize, next_cup: usize) {
+    fn set_next_cup(&mut self, cup: usize, next_cup: usize) {
         self.next[cup - 1] = next_cup;
     }
 
-    fn iter_from(&self, cup: usize) -> IterCups<'_> {
-        IterCups {
+    fn iter_from(self, cup: usize) -> Self {
+        Self {
             current: cup,
-            next: &self.next,
+            ..self
         }
     }
 
-    fn perform_move(&mut self) {
-        // pick up the cups
-        let pickup1 = self.next(self.current);
-        let pickup2 = self.next(pickup1);
-        let pickup3 = self.next(pickup2);
-        self.set_next(self.current, self.next(pickup3));
-        // find the destination
-        let dest_cup = itertools::iterate(self.current, |&cup| dec_cup(cup, self.next.len()))
+    fn play_moves(mut self, move_count: usize) -> Self {
+        for _ in 1..=move_count {
+            self.perform_move();
+        }
+        self
+    }
+
+    fn from_slice(slice: &[usize]) -> Self {
+        let current = slice[0];
+        let mut next: Vec<_> = slice.into();
+        let cups = slice.iter().copied();
+        let cups_shifted = slice.iter().copied().skip(1).chain(iter::once(slice[0]));
+        for (cup, next_cup) in cups.zip(cups_shifted) {
+            next[cup - 1] = next_cup;
+        }
+        Cups { current, next }
+    }
+
+    fn find_dest(&self, picked_up: &[usize]) -> usize {
+        itertools::iterate(self.current, |&cup| dec_cup(cup, self.next.len()))
             .skip(1)
-            .find(|cup| ![pickup1, pickup2, pickup3].contains(cup))
-            .unwrap();
-        // put down the cups
-        self.set_next(pickup3, self.next(dest_cup));
-        self.set_next(dest_cup, pickup1);
+            .find(|cup| !picked_up.contains(cup))
+            .unwrap()
+    }
+
+    fn perform_move(&mut self) {
+        // pick up cups
+        let pickup1 = self.next_cup(self.current);
+        let pickup2 = self.next_cup(pickup1);
+        let pickup3 = self.next_cup(pickup2);
+        self.set_next_cup(self.current, self.next_cup(pickup3));
+        // find destination
+        let dest_cup = self.find_dest(&[pickup1, pickup2, pickup3]);
+        // put down cups
+        self.set_next_cup(pickup3, self.next_cup(dest_cup));
+        self.set_next_cup(dest_cup, pickup1);
         // advance current
-        self.current = self.next(self.current);
+        self.current = self.next_cup(self.current);
     }
 }
 
@@ -92,19 +99,15 @@ impl<'a> Day<'a> for Day23 {
     }
 
     fn solve_part1(input: Self::Input1) -> (Self::Input2, Self::Output1) {
-        let mut cups = Cups::from_slice(&input);
-        for _ in 1..=100 {
-            cups.perform_move();
-        }
-        (
-            input,
-            cups.iter_from(1)
-                .skip(1)
-                .take(8)
-                .zip(itertools::iterate(10000000, |n| n / 10))
-                .map(|pair| pair.0 * pair.1)
-                .sum(),
-        )
+        let output = Cups::from_slice(&input)
+            .play_moves(100)
+            .iter_from(1)
+            .skip(1)
+            .take(8)
+            .zip(itertools::iterate(10000000, |n| n / 10))
+            .map(|pair| pair.0 * pair.1)
+            .sum();
+        (input, output)
     }
 
     fn solve_part2(mut input: Self::Input2) -> Self::Output2 {
@@ -112,10 +115,11 @@ impl<'a> Day<'a> for Day23 {
         for i in 10..=1000000 {
             input.push(i);
         }
-        let mut cups = Cups::from_slice(&input);
-        for _ in 1..=10000000 {
-            cups.perform_move();
-        }
-        cups.iter_from(1).skip(1).take(2).product()
+        Cups::from_slice(&input)
+            .play_moves(10000000)
+            .iter_from(1)
+            .skip(1)
+            .take(2)
+            .product()
     }
 }
